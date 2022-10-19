ee257a27c76c6a945a3b3b4d67754deda4dfe156
RenatoCaushi <ren.caushi22@gmail.com>
2022-10-19 14:00:08 +0200

    first commit finished

---
+++ demo.py
@@ -0,0 +1,32 @@
+for x in range(0, 151, 1):
+    print(x)
+
+for x in range(5, 1001):
+    if x%5 0= 0:
+        print(x)
+
+        
+for x in range(1, 101,1):
+    if x % 10 == 0:
+        print('Coding Dojo')
+    elif x % 5 == 0:
+        print('Coding')
+    else:
+        print(x)
+
+sum = 0    
+for x in range(500001):
+    if x%2 != 0:
+        print(x)
+        sum = sum +x
+print(sum)
+
+for nr in range(2018, -1, -4):
+    print(nr)
+
+lowNum =2 
+highNum =9
+mult =3
+for nr in range(lowNum, highNum+1, 1):
+    if nr % mult == 0:
+        print(nr)
